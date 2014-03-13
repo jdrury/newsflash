044ae57cce08e82ca953399581694a7954c74706
James Drury <jdrury86@gmail.com>
2014-03-13 10:18:06 -0400

    fixes alchemyapi call, but my api key is banned

--- nytimesapi.js
+++ nytimesapi.js
@@ -1,8 +1,5 @@
 var http       = require('http')
-  , Promise    = require('bluebird')
-  , AlchemyAPI = require('./alchemyapi')
-  , alchemyapi = new AlchemyAPI()
-  , self       = require('./nytimesapi.js');
+  , Promise    = require('bluebird');
 
 // nytimes api
 var nytimes_key = '9f827b34ac633dc815206c8dab6ff00b:3:56570661'
@@ -11,27 +8,30 @@ var nytimes_key = '9f827b34ac633dc815206c8dab6ff00b:3:56570661'
       path: "/svc/news/v3/content/all/all.json?&limit=50&api-key=" + nytimes_key
     };
 
+// alchemy api
+var AlchemyAPI = require('./alchemyapi')
+  , alchemyapi = new AlchemyAPI();
+
+
 exports.pullArticles = function(callback) {
-  // return new Promise(function(resolve, reject) {
-    // establish connection with NYTimes API
+  // establish connection with NYTimes API
   http.get(options, function(res) {
-    var news = ""
+    var data     = ""
       , articles = [];
 
     res.on('data', function(chunk) {
-      news += chunk;
+      data += chunk;
     });
 
     // save all the articles in the newswire
     res.on('end', function() {
       if (res.statusCode === 200) {
-        var pretty_news = JSON.parse(news)
+        var pretty_news = JSON.parse(data)
           , articles    = pretty_news.results;
       }
 
       console.log("Pulling down " + articles.length + " NYTimes articles...")
 
-      console.log(articles)
       callback(articles);
     });
   });
@@ -40,30 +40,27 @@ exports.pullArticles = function(callback) {
 exports.getKeywords = function() {
   return new Promise(function(resolve, reject) {
     exports.pullArticles(function(articles) {
-      articles.forEach(function(article) {
-        var text     = ""
-          , cauldron = {}
-          , keywords = [];
+      var content  = ""
+        , cauldron = {}
+        , keywords = [];
 
+      articles.forEach(function(article) {
         // pluck the abstracts from each article
         abstract = article.abstract;
-        text += abstract + " ";
+        content += abstract + " ";
+      });
 
-        console.log(text)
-        // define the keywords of each abstract
-        // alchemyapi.keywords("text", text, {}, function(response) {
-        //   cauldron['keywords'] = { text:text, response:JSON.stringify(response,null,4), results:response['keywords'] };
-        //   console.log(cauldron.keywords.results)
-        //   // sort through returned object for the data we want
-        //   cauldron.keywords.results.forEach(function(element) {
-        //     for(var key in element) {
-        //       if (key === 'text') {
-        //         keywords.push(element[key]);
-        //       }
-        //     }
-        //   });
-          resolve(keywords);
-        // });
+      alchemyapi.keywords('text', content, { 'sentiment':1 }, function(response) {
+        cauldron['keywords'] = { text:content, response:JSON.stringify(response,null,4), results:response['keywords'] };
+
+        cauldron.keywords.results.forEach(function(element) {
+          for(var key in element) {
+            if (key === 'content') {
+              keywords.push(element[key]);
+            }
+          }
+        });
+        resolve(keywords);
       });
     });
   });
