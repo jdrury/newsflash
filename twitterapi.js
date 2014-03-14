0e7c9cf1ec20bdafc89ee0a2e24b7172f01f7d4c
James Drury <jdrury86@gmail.com>
2014-03-14 17:32:15 -0400

    adds static d3 treemap. need to add dynamic adjustment and colors

--- twitterapi.js
+++ twitterapi.js
@@ -3,11 +3,14 @@ var nytimes = require('./nytimesapi.js')
   , Promise = require('bluebird')
   , _       = require('underscore');
 
-var watchKeywords = [];
 
 var watchList = {
+  name: "NewsFeed",
   total: 0,
-  keywords: {}
+  watchKeywords: [],
+  children: [
+
+  ]
 };
 
 var t = new twitter({
@@ -24,13 +27,25 @@ exports.initializeFeed = function() {
     nytimes.getKeywords().then(function(keyterms) {
       // add the alchemy keywords to the watchKeywords array
 
-      _.each(keyterms, function(keyterm) {
-        watchKeywords.push(keyterm);
-      });
+      keyterms.forEach(function(e) {
+        var obj = { name: e, size: 0 };
+        watchList.children.push(obj);
+        watchList.watchKeywords.push(e);
+      })
+
+      // watchList.children.forEach(function())
+      // _.each(keyterms, function(keyterm) {
+      //   watchKeywords.push(keyterm);
+      // });
 
       // initialize keywords at zero
-      _.each(watchKeywords, function(e) { watchList.keywords[e] = 0; });
-
+      // _.each(watchKeywords, function(e) {
+      //   watchList.keywords[e] = 0;
+      //   // watchList.children
+      // });
+      // console.log(watchList.watchKeywords)
+      // console.log(watchList)
+      console.log(watchList)
       resolve(watchList);
     });
   });
@@ -39,15 +54,15 @@ exports.initializeFeed = function() {
 // Compares watchList (NYT Keywords) to Twitter stream, counts every mention
 exports.keywordStream = function(callback) {
   exports.initializeFeed().then(function(watchList) {
-    t.stream('statuses/filter', { track: watchKeywords }, function(stream) {
+    t.stream('statuses/filter', { track: watchList.watchKeywords }, function(stream) {
       // read twitter firehose ('data') for incoming tweets.
       stream.on('data', function(tweet) {
         var tweetText = tweet.text.toLowerCase();
         // compare the text of each tweet to each NYT keyword in the watchList
-        _.each(watchKeywords, function(e) {
+        _.each(watchList.children, function(e) {
           // if the keyword exists in the tweet, += 1
-          if (tweetText.indexOf(e.toLowerCase()) !== -1) {
-            watchList.keywords[e] += 1;
+          if (tweetText.indexOf(e.name.toLowerCase()) !== -1) {
+            e.size += 1;
             watchList.total += 1;
             callback(watchList);
           }
