4e6a93164e965048dbdc2ecf0a68cea346fc7783
James Drury <jdrury86@gmail.com>
2014-03-31 08:56:27 -0400

    switch to swig templating

--- config/keys/nytimesapi.js
+++ config/keys/nytimesapi.js
@@ -4,5 +4,5 @@ var nytNewswireKey = '24995b8ee4501f3b552ed25e8a608801:10:68945244';
 
 exports.options = {
     host: "api.nytimes.com"
-  , path: "/svc/news/v3/content/all/all.json?&limit=50&api-key=" + nytNewswireKey
+  , path: "/svc/news/v3/content/all/all.json?&limit=5&api-key=" + nytNewswireKey
 };
--- server.js
+++ server.js
@@ -2,12 +2,16 @@ var express  = require('express')
   , app      = express()
   , path     = require('path')
   , server   = require('http').createServer(app)
+  , swig     = require('swig')
   , io       = require('socket.io').listen(server)
   , firehose = require('./config/scripts/firehose.js');
 
+app.engine('html', swig.renderFile);
+
 app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/app/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -15,7 +19,7 @@ app.use(express.errorHandler());
 app.use(express.urlencoded());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-// app.use(app.router);
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'app/public')));
 
 app.get('/', function(req, res) {
